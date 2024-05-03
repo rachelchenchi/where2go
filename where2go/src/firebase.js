32d8a66b0f1e051e84c80dee4077254436bd1c23
kzhuoxu <xuzhuoqi2019@outlook.com>
2024-05-03 17:50:47 -0400

    added google analytics

--- where2go/src/firebase.js
+++ where2go/src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -8,8 +9,10 @@ const firebaseConfig = {
     projectId: "cs5356-where2go",
     storageBucket: "cs5356-where2go.appspot.com",
     messagingSenderId: "867159952646",
-    appId: "1:867159952646:web:c3aab8840f229b9e95e287"
+    appId: "1:867159952646:web:c3aab8840f229b9e95e287",
+    measurementId: "G-C23RP8K06Q"
 };
+
 
 let firebaseApp
 if (!getApps().length) {
@@ -17,5 +20,7 @@ if (!getApps().length) {
 } else {
     firebaseApp = getApps()[0];
 }
+
+const analytics = getAnalytics(firebaseApp);
 
 export default firebaseApp;
