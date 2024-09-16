fe0479913443277ca76dc1ccc0285f45ca9bb751
chen chi <rachelchenchi@163.com>
2024-09-15 21:05:09 -0400

    footer change

--- where2go/src/components/Footer.js
+++ where2go/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="content has-text-centered">
                 <p>
                     <strong>WHERE2GO</strong> for{" "}
-                    <a href="https://jgthms.com">WHERE2GO</a>.
+                    <a href="https://github.com/rachelchenchi/where2go">WHERE2GO</a>.
                 </p>
             </div>
         </footer>
